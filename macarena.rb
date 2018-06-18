03a1410c34d8edf49f289f206c7b0c116b2cc4a9
Grace Govan <ggovan11@outlook.com>
2018-06-18 19:46:13 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -22,7 +22,7 @@ puts "Step 5: Place your right hand on your left shoulder."
 # Step 6: FIXME
 puts "Step 6: "Place your left hand on your right shoulder."
 # Step 7: FIXME
-puts "Step 7: "Place your left hand on your right shoulder."
+puts "Step 7: "Bring your "
 
 # Step 8: FIXME
 puts "Step 8: The Last Step"
